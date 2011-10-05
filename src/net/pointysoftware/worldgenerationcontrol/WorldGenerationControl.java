266b5723f5d7a34e8c388a2cfe2eb06f31a2b1d7
John Schoenick <john@pointysoftware.net>
2011-10-05 14:12:04 -0700

    Release v2.1

--- src/net/pointysoftware/worldgenerationcontrol/WorldGenerationControl.java
+++ src/net/pointysoftware/worldgenerationcontrol/WorldGenerationControl.java
@@ -55,7 +55,7 @@ import org.bukkit.scheduler.BukkitScheduler;
 
 public class WorldGenerationControl extends JavaPlugin implements Runnable
 {
-    private final static String VERSION = "2.0";
+    private final static String VERSION = "2.1";
     public enum GenerationSpeed
     {
         // Do everything on the same tick, locking up
